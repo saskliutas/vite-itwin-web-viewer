84261bf656c797088096f1da74bc98fe1ae8cf6e
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-07-15 18:24:26 +0300

    Restructure app

--- src/index.tsx
+++ src/index.tsx
@@ -7,84 +7,8 @@ import "./index.css";
 import "@itwin/itwinui-react/styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ErrorBoundary } from "react-error-boundary";
-import { SvgError } from "@itwin/itwinui-illustrations-react";
-import { NonIdealState, ThemeProvider } from "@itwin/itwinui-react";
-import {
-  createRootRoute,
-  createRoute,
-  createRouter,
-  Outlet,
-  RouterProvider,
-} from "@tanstack/react-router";
-import { App } from "./components/App";
-import { AuthorizationProvider, SignInRedirect } from "./Authorization";
-
-const rootRoute = createRootRoute({
-  component: () => (
-    <ThemeProvider theme="light">
-      <ErrorBoundary
-        FallbackComponent={({ error }) => (
-          <NonIdealState
-            svg={<SvgError />}
-            heading={"An error occurred"}
-            description={error.message}
-          />
-        )}
-      >
-        <AuthorizationProvider>
-          <Outlet />
-        </AuthorizationProvider>
-      </ErrorBoundary>
-    </ThemeProvider>
-  ),
-});
-
-interface IndexSearchParams {
-  iTwinId: string;
-  iModelId: string;
-  changesetId?: string;
-}
-
-const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  validateSearch: (search: Record<string, unknown>): IndexSearchParams => {
-    const iTwinId =
-      (search.iTwinId as string | undefined) ?? import.meta.env.IMJS_ITWIN_ID;
-    const iModelId =
-      (search.iModelId as string | undefined) ?? import.meta.env.IMJS_IMODEL_ID;
-    const changesetId = search.changesetId as string | undefined;
-    if (!iTwinId || !iModelId) {
-      throw new Error(
-        "Please add a valid iTwin ID and iModel ID in the .env file and restart the application or add it to the `iTwinId`/`iModelId` query parameter in the url and refresh the page. See the README for more information."
-      );
-    }
-    return {
-      iTwinId,
-      iModelId,
-      changesetId,
-    };
-  },
-  path: "/",
-  component: function Index() {
-    const { iTwinId, iModelId } = indexRoute.useSearch();
-    return <App iTwinId={iTwinId} iModelId={iModelId} />;
-  },
-});
-
-const signinRedirectRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/signin-callback",
-  component: SignInRedirect,
-});
-
-const routeTree = rootRoute.addChildren([indexRoute, signinRedirectRoute]);
-
-const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
-  scrollRestoration: true,
-});
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./components/Routes";
 
 declare module "@tanstack/react-router" {
   interface Register {
